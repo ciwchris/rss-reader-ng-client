3741d36a2469ce8b5b220d651b6802574c8cb4df
Christopher Lopes <christopher.lopes@gmail.com>
2017-04-11 22:07:09 -0700

    Add bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ReversePipe } from 'ngx-pipes/src/app/pipes/array/reverse';
+
 import { FeedServiceService } from './feed-service.service'
 import { Feeds } from './feeds'
 import { FeedContent } from './feed-content'
@@ -8,21 +10,42 @@ import { FeedContent } from './feed-content'
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    providers: [FeedServiceService]
+    providers: [FeedServiceService, ReversePipe]
 })
 
 export class AppComponent {
     public feeds: Feeds;
+    public hasError: boolean;
+    public hasResult: boolean;
+    public isSubmitted: boolean;
 
     constructor(private feedServiceService: FeedServiceService) {
-        this.feedServiceService
-            .getContent()
-            .subscribe(feeds => {
-                console.log(feeds);
-                this.feeds = feeds;
-            }, error => {
-                console.log(error);
-            });
+        this.hasError = false;
+        this.hasResult = false;
+        this.isSubmitted = false;
+
+        const localFeeds = localStorage.getItem('feeds');
+
+        if (localFeeds === null) {
+
+            this.isSubmitted = true;
+            this.feedServiceService
+                .getContent()
+                .subscribe(feeds => {
+                    console.log(feeds);
+                    localStorage.setItem('feeds', JSON.stringify(feeds));
+                    this.feeds = feeds;
+                    this.hasResult = true;
+                    this.isSubmitted = false;
+                }, error => {
+                    console.log(error);
+                    this.hasError = true;
+                    this.isSubmitted = false;
+                });
+        } else {
+            console.log('loading from cache');
+            this.hasResult = true;
+            this.feeds = JSON.parse(localFeeds);
+        }
     }
-    title = 'app works!';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,22 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { NgArrayPipesModule } from 'ngx-pipes';
+
 import { AppComponent } from './app.component';
 import { FeedServiceService } from './feed-service.service';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [FeedServiceService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        NgArrayPipesModule
+    ],
+    providers: [FeedServiceService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
